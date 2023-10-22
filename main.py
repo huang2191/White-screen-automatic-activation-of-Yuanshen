bf73bf05e715bd25da08de15dc1cf638173eb19c
huang2191 <110579349+huang2191@users.noreply.github.com>
2023-10-22 11:59:35 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,121 @@
+mun = 0
+
+
+def land():
+    global mun
+    mun = int(mun + 1)
+    a = mun / 12
+    a = "%.2f" % a
+    print("\r正在载入第三方库", "[", a, "]", end='')
+
+
 import subprocess
+
+land()
 import time
 
+land()
 import cv2
+
+land()
 import numpy as np
+
+land()
 import pyautogui
+
+land()
 from PIL import ImageGrab
 
+land()
+import threading
 
-def new_play_video():
+land()
+from playsound import playsound
+#import mp3play
+land()
+import win32gui
+
+land()
+import win32con
+
+land()
+import win32com.client
+
+land()
+import pythoncom
+
+land()
+print("\r载入第三方库完毕""\n""开始载入视频", end=" ")
+cap = cv2.VideoCapture("video.mp4")
+# 打开我们要播放的文件
+print("\r载入视频完成，开始检测")
+
+
+def run_top():
+    #奇怪的bug，这个param必须有，否则报错
+   # def get_all_hwnd(hwnd, param):
+    def get_all_hwnd(hwnd, param):
+        if win32gui.IsWindow(hwnd) and win32gui.IsWindowEnabled(hwnd) and win32gui.IsWindowVisible(hwnd):
+            hwnd_map.update({hwnd: win32gui.GetWindowText(hwnd)})
+
+    hwnd_map = {}
+    win32gui.EnumWindows(get_all_hwnd, 0)
+
+    for h, t in hwnd_map.items():
+        if t:
+            if t == 'video':
+                win32gui.BringWindowToTop(h)
+                pythoncom.CoInitialize()
+                shell = win32com.client.Dispatch("WScript.Shell")
+                shell.SendKeys('%')
+
+                win32gui.SetForegroundWindow(h)
+
+                win32gui.ShowWindow(h, win32con.SW_RESTORE)
+
+
+def video():
+    cap.get(cv2.CAP_PROP_FPS)
+    while True:
+        ret, frame = cap.read()
+        cv2.namedWindow("video", cv2.WINDOW_NORMAL)
+        cv2.setWindowProperty("video", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        cv2.imshow("video", frame)
+        run_top()
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+
+    cv2.destroyAllWindows()
+
+
+def music():
+    playsound("video.mp3")
+
+
+
+def run():
+    vd = threading.Thread(target=video)
+    mc = threading.Thread(target=music)
+    vd.start()
+    mc.start()
+
+
+def old_play_video():
+    # 加大力度
+    pyautogui.hotkey('esc')
+    # 模拟按下Alt+F4组合键来关闭当前活动窗口
+    pyautogui.hotkey('alt', 'f4')
+    # 播放视频
     proc = subprocess.Popen(['start', 'Video.mp4'], shell=True)
     proc.communicate()
     # 自动点击
-    # 模拟按下Alt+F4组合键来关闭当前活动窗口
-    pyautogui.hotkey('alt', 'f4')
     # 将鼠标移动到屏幕右上角
     pyautogui.moveTo(screenWidth, 100)
-    time.sleep(1)
+    time.sleep(0.8)
     pyautogui.hotkey('F11')
+    pyautogui.click()
     time.sleep(15)
     pyautogui.hotkey('alt', 'F4')
 
@@ -39,38 +138,35 @@ def detect_white_screen():
 
 
 # 原神启动函数
-def yuanshen_start(start_model, yuanshen_lnk):
+def yuanshen_start(start_model):
     if start_model == 1:
-        subprocess.run(yuanshen_lnk)
+        pass
     else:
-        new_play_video()
-
-
+        run()
+run()
 # 获取屏幕分辨率
 screenWidth, screenHeight = pyautogui.size()
-print("屏幕分辨率为 %sx%s" % (screenWidth, screenHeight))
-yuanshen_lnk = None
+# print("屏幕分辨率为 %sx%s" % (screenWidth, screenHeight))
 # 询问是否有安装原神
 start_model = 'none'
 
-while True:
-    model = input("是否有安装原神？（yes/no） ")
-    if model == "yes":
-        start_model = 1
-        break
-    elif model == "no":
-        start_model = 0
-        break
-    else:
-        print("请输入yes/no")
-        continue
-print("启动模式为" + str(start_model))
-# 询问原神ink(如果有）
-if start_model == 1:
-    yuanshen_lnk = input("请原神输入lnk")
-else:
-    pass
-
+# while True:
+#     model = input("是否有安装原神？（yes/no） ")
+#     if model == "yes":
+#         start_model = 1
+#         break
+#     elif model == "no":
+#         start_model = 0
+#         break
+#     else:
+#         print("请输入yes/no")
+#         continue
+# print("启动模式为" + str(start_model))
+# # 询问原神ink(如果有）
+# if start_model == 1:
+#     yuanshen_lnk = input("请原神输入lnk")
+# else:
+#     pass
 
 # 是否有白屏（快速）
 while True:
@@ -80,19 +176,19 @@ while True:
     a2 = min(pix[screenWidth - 1, 1])
     b1 = min(pix[1, screenHeight - 1])
     b2 = min(pix[screenWidth - 1, screenHeight - 1])
-    print("屏幕四个角的RGB值的最小值：", a1, a2, b1, b2)
+    print("\r屏幕四个角的RGB值的最小值：", a1, a2, b1, b2, end='')
     s = a1 + a2 + b1 + b2
-    print(s)
     if s >= 900:
         s1 = detect_white_screen()
         if s1 >= 254:
             print("原神，启动！")
-            yuanshen_start(start_model, yuanshen_lnk)
+            yuanshen_start(start_model)
             break
         else:
-            print("快速检测失误")
+            print("\r快速检测失误")
+            time.sleep(0.1)
             continue
     else:
-        time.sleep(0.5)
+        time.sleep(0.05)
         continue
 pass
